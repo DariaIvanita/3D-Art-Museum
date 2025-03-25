aba42d94a5abf4d21528a34c63ec19a1481afb64
DariaIvanita <147073195+DariaIvanita@users.noreply.github.com>
2025-03-24 23:52:07 -0600

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,4 +1,3 @@
-
 // Scene setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -147,9 +146,6 @@ function animate() {
 }
 
 animate();
-
-
-
 
 
 
